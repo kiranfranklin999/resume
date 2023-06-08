6f58912b815c0f48a831c6cea66130443ce10f6e
kf154363 <kiran.x.franklin@gsk.com>
2023-06-08 10:10:27 +0530

    updated the new changes

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ $('#card-1').deAnimate({
     classIn: 'flipInX',
     parallel: false
   });
+
+$(document).ready(function() {
+    $('.slider').slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      prevArrow: $('.slider-nav .prev'),
+      nextArrow: $('.slider-nav .next')
+    });
+  });
